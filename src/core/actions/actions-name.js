adf3c573ce905077bac722f662a3492ee04a2d9f
dhagell <>
2021-04-16 11:33:41 -0400

    updated

--- src/core/actions/actions-name.js
+++ src/core/actions/actions-name.js
@@ -1,19 +1,19 @@
 import constants   from 'core/types'
 import contract    from 'truffle-contract'
-import AmazingDapp from 'contracts/AmazingDapp.json'
+import HashArtDapp from 'contracts/hashartERC1155.json'
 
 export function checkIfNameExists(name) {
   return (dispatch, getState) => {
     const { web3Provider } = getState().provider
-    const AmazingDappContract = contract(AmazingDapp)
+    const HashArtDappContract = contract(HashArtDapp)
 
-    AmazingDappContract.setProvider(web3Provider.currentProvider)
-    AmazingDappContract.defaults({ from: web3Provider.eth.defaultAccount })
-    // alert(AmazingDapp.contractName)
+    HashArtDappContract.setProvider(web3Provider.currentProvider)
+    HashArtDappContract.defaults({ from: web3Provider.eth.defaultAccount })
+    // alert(HashArtDapp.contractName)
 
     return new Promise((resolve, reject) => {
-      AmazingDappContract.deployed().then((ad) => {
-        return ad.checkIfExist(name)
+      HashArtDappContract.deployed().then((ad) => {
+        return ad.exists(name)
       }).then((result) => {
         resolve(result)
         reject(new Error('Error message!', result))
